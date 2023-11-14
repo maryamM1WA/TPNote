54aa809f8081422f5bcc71dd6e6d7aecbbead746
Maryam EID <eid.maryam@hotmail.com>
2023-11-14 23:05:01 +0100

    affichage
    affichage des nom et prenom des clients

--- interfaceBanque.php
+++ interfaceBanque.php
@@ -5,6 +5,31 @@
     <title>Interface de la banque</title>
   </head>
   <body>
+<h1>Liste des Clients</h1>
 
+
+
+<?php
+require_once('C:\Users\eidma\OneDrive\Bureau\XAMPP\htdocs\TPNote\BO\clientBO.php');
+
+//Création de l'objet clientBO
+
+$clientBO=new ClientBO();
+
+//Récupèration des noms et prenoms des Clients
+
+$listeClient=$clientBO->getClients();
+
+//Affichage de la listeClient
+
+echo"<ul>";
+foreach ($listeClient as $client) {
+  echo"<li>";
+  echo "<p>{$client->id_client} {$client->nom} {$client->prenom}</p>";
+}
+echo "</ul>";
+
+
+ ?>
   </body>
 </html>
