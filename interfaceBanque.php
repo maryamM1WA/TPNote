9d08d6ffa3b66473889254b803c7f5a72e5397ba
Maryam EID <eid.maryam@hotmail.com>
2023-11-14 23:47:17 +0100

    création
    affichage et style de la page

--- interfaceBanque.php
+++ interfaceBanque.php
@@ -6,7 +6,7 @@
   </head>
   <body>
 <h1>Liste des Clients</h1>
-
+<div id="detailClient" class="details"></div>
 
 
 <?php
@@ -24,12 +24,41 @@ $listeClient=$clientBO->getClients();
 
 echo"<ul>";
 foreach ($listeClient as $client) {
-  echo"<li>";
-  echo "<p>{$client->id_client} {$client->nom} {$client->prenom}</p>";
+  echo"<li class='client' data-id='{$client->id_client}'>";
+  echo "<p> {$client->nom} {$client->prenom}</p>";
+
+//bloc contient les details client
+
+echo "<div class='details'>";
+echo "<p>ID:{$client->id_client}</p>";
+echo "<p>Date de naissance: {$client->ddn}</p>";
+echo "<p>Statut matrimonial: {$client->statut_matrimonial}</p>";
+echo "<p>Nombre d'enfants: {$client->nbEnfants}</p>";
+echo "<p>Statut professionnel: {$client->statutPro}</p>";
+echo "</div>";
+
+
+echo "</li>"
+
 }
 echo "</ul>";
-
-
  ?>
+
+ <script >
+
+ // Écouteur d'événement au clic sur un client
+     document.querySelectorAll('.client').forEach(function(client) {
+         client.addEventListener('click', function() {
+             // Trouver la div de détails du client liée à ce client
+             var detailsDiv = this.querySelector('.details');
+
+             // Inverser le style 'display' entre 'block' et 'none'
+             detailsDiv.style.display = (detailsDiv.style.display === 'block') ? 'none' : 'block';
+         });
+     });
+
+
+}
+ </script>
   </body>
 </html>
